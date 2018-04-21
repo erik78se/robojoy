2b09e0aa70e62490b1923a2bb03dd7a5363484c7
Jimmy Hedman <jimmy.hedman@gmail.com>
2018-04-21 15:51:15 +0200

    Source ROS setup.bash.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,6 +10,8 @@ GROUPID="$(stat -c '%g' "$ROBOTREPO")"
 groupadd -g "$GROUPID" "$GROUP" 2> /dev/null
 useradd -o -m --uid "$USERID" --gid "$GROUPID" "$USER"
 echo "$USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/user
+# FIXME: Should be variable
+echo "source /opt/ros/kinetic/setup.bash" >> /home/robot/.bashrc
 
 # shellcheck disable=SC2068
 exec su "$USER" $@
